93f8491de2b8b61314a7026424b5582fccebd8dc
Ismael Silván Vicente <silvanmanager@gmail.com>
2023-08-16 17:11:04 +0200

    Creando nuevo proyecto para seguir aprendiendo React y primer ejercicio de prueba realizado

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// import MiPrimerEstado from "./componentes/MiPrimerEstado";
+import RepasoComponent from "./componentes/Ejercicio repaso/RepasoComponent";
 
 function App() {
+ 
+const yearActual = new Date().getFullYear();
+ 
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>El estado en React - Hok useState</h1>
+        {/* <MiPrimerEstado /> */}
+        <RepasoComponent yearActual={yearActual}/> 
       </header>
     </div>
   );
---
+++ src/componentes/Ejercicio repaso/RepasoComponent.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const RepasoComponent = ({ yearActual }) => {
+  const [yearIndicado, setYear] = useState(yearActual);
+
+  const mas_menos_1 = (incremeto) => {
+    setYear(yearIndicado + incremeto);
+    console.log(yearIndicado);
+  };
+
+  const cambiarYear = (numero) => {
+    setYear(parseInt(numero));
+  };
+
+  return (
+    <div>
+      <h3>RepasoComponent</h3>
+      <div className="comprobacion-caja">
+        <span >- {yearIndicado} -</span>
+      </div>
+      <div className="botones-calculo-year">
+        <button id="restar-year" onClick={() => mas_menos_1(-1)}>
+          Restar <strong>-1</strong> año
+        </button>
+        <button id="sumar-year" onClick={() => mas_menos_1(1)}>
+          Sumar <strong>+1</strong> año
+        </button>
+      </div>
+      <div id="form-year">
+        <label for="year-input">Indica un año para cambiarlo</label>
+        <input
+          id="year-input"
+          type="number"
+          onChange={(e) => cambiarYear(e.target.value)}
+          placeholder="____________"
+        />
+      </div>
+  
+    </div>
+  );
+};
+
+// RepasoComponent.propTypes = {
+//     yearActual: PropTypes.number.isRequired
+// };
+
+export default RepasoComponent;
