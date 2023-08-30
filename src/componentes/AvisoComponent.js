42ad68edd82f9620b9c3187184520d2f32f2c49a
Ismael Silván Vicente <silvanmanager@gmail.com>
2023-08-30 14:34:14 +0200

    hooks y useEffects aprendidos

---
+++ src/componentes/AvisoComponent.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+
+export const AvisoComponent = () => {
+  useEffect(() => {
+    //   Cuando se monta el componente
+    alert("El componente AVISOCOMPONENTE esta MONTADO");
+
+    // Cuando se desmonta el componente
+    return () => {
+      alert("Componente desmontado");
+    };
+  }, []);
+
+  return (
+    <div>
+      <hr />
+      <hr />
+      <hr />
+      <h2>
+        <strong>Hola creador</strong>
+      </h2>
+      <button onClick={() => alert("Bienvenido")}>Mostrar alerta</button>
+    </div>
+  );
+};
--- src/componentes/PruebasComponent.js
+++ src/componentes/PruebasComponent.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { AvisoComponent } from "./AvisoComponent";
 
 const PruebasComponent = () => {
   const [usuario, setUsuario] = useState("Ismael Silván");
   const [fecha, setFecha] = useState(new Date().toLocaleString());
+  const [contador /* , setContador */] = useState(0);
 
   const modUsuario = (e) => {
     setUsuario(e.target.value);
@@ -18,17 +20,25 @@ const PruebasComponent = () => {
   useEffect(() => {
     console.log("Has hecho un cambio en un estado");
   });
+  // Al cargar el componente
   useEffect(() => {
     console.log("Has cargado el componente");
-  },[]);
+  }, []);
+  // Al cambiar una variable
   useEffect(() => {
-    console.log("Has hecho un cambio en usuario");
-  },[usuario]);
+    // setContador(contador+1);
+    // console.log("Has hecho un cambio en el usuario " + contador + " veces");
+    console.log("Has hecho un cambio en el usuario ");
+  }, [usuario, fecha]);
 
   return (
     <div>
       <h1> El efecto - Hook useEffect</h1>
-      <div className="comprobacion-caja">
+      <div
+        className={
+          contador <= 10 ? "comprobacion-caja" : "comprobacion-caja opcion-2"
+        }
+      >
         <p>
           {usuario} - {fecha}
         </p>
@@ -43,6 +53,8 @@ const PruebasComponent = () => {
       <div>
         <button onClick={changeFecha}>Cambiar fecha</button>
       </div>
+
+      {usuario === "Ismael Silván Vicente" && <AvisoComponent />}
     </div>
   );
 };
